76c34758b8d32ae0dbf0083d41e317c58728786c
YuriyOrlov <navuchodonsr@yandex.ru>
2017-04-18 18:14:27 +0300

    Finished first working version of a file. It fully functional.

--- duplicates.py
+++ duplicates.py
@@ -1,10 +1,11 @@
 import os
 from sys import argv
-from itertools import combinations
+import itertools
 from filecmp import cmp
+from args_parser import ConsoleArgsParser
 
 
-def scantree(path, depth=5):
+def scantree(path, depth):
     for entry in os.scandir(path):
         if not entry.name.startswith('.') and entry.is_dir(follow_symlinks=False) and depth:
             yield from scantree(entry.path, depth - 1)
@@ -13,27 +14,28 @@ def scantree(path, depth=5):
 
 
 def get_duplicates(files):
-    files_comb = [item for item in combinations(files, 2)]
-    combinations_list = [[file1, file2] for file1, file2 in files_comb if cmp(file1, file2, True)]
-    return combinations_list
+    files_comb = itertools.combinations(files, 2)
+    return [[file1, file2] for file1, file2 in files_comb if cmp(file1, file2, True)]
 
 
-def has_input_length(argv):
-    if len(argv) > 2:
-        return argv[1], int(argv[2])
-    elif len(argv) > 1:
-        return argv[1], 5
-    else:
-        return '.', 5
+def writing_duplicates_into_file(duplicates_list):
+    with open('duplicated_files.txt', 'w') as duplicates:
+        for file_with_path in duplicates_list:
+            duplicates.write('{} <---> {}\n'.format(file_with_path[0], file_with_path[1]))
 
 
 if __name__ == '__main__':
-    path_to_begin, recursion_limitation = has_input_length(argv)
+    args_parser = ConsoleArgsParser()
+    args = args_parser.parse_args()
+    path_to_begin, recursion_limitation, form_of_result_representation = args.folder, args.depth, args.result
     files_from_subfolders = [file.path for file in scantree(path_to_begin, recursion_limitation)]
     duplicates_list = get_duplicates(files_from_subfolders)
-    if duplicates_list:
+    if duplicates_list and (form_of_result_representation == 'scr'):
         print('\nDuplicates found\n')
         for file_with_path in duplicates_list:
             print('{} <---> {}'.format(file_with_path[0], file_with_path[1]))
+    elif duplicates_list and (form_of_result_representation == 'file'):
+        writing_duplicates_into_file(duplicates_list)
+        print('File «duplicated_files.txt» created.')
     else:
         print('No duplicates found.')
